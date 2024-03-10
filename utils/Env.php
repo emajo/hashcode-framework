787344c416d422373cb4eecc78a7c815cd5a4fec
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2024-03-10 11:22:05 +0100

    added global consts util

---
+++ utils/Env.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Utils;
+
+class Env
+{
+    public const FILES_DIR = 'files/';
+
+    public const INPUT_DIR = self::FILES_DIR . 'input/';
+    public const OUTPUT_DIR = self::FILES_DIR . 'output/';
+    public const LOGS_DIR =  self::FILES_DIR . 'logs/';
+}
