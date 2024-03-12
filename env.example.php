01fe440eb1f28256a7c74e056cfe1c0bce5591fb
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2024-03-12 20:38:23 +0100

    added environment variables management

---
+++ env.example.php
@@ -0,0 +1,4 @@
+<?php
+
+define('TELEGRAM_BOT_TOKEN', '');
+define('TELEGRAM_CHAT_ID', '');
--- utils/Env.php
+++ utils/Env.php
@@ -2,6 +2,9 @@
 
 namespace Utils;
 
+require_once __DIR__ . "/../env.php";
+
+
 class Env
 {
     public const FILES_DIR = 'files/';
@@ -9,4 +12,9 @@ class Env
     public const INPUT_DIR = self::FILES_DIR . 'input/';
     public const OUTPUT_DIR = self::FILES_DIR . 'output/';
     public const LOGS_DIR =  self::FILES_DIR . 'logs/';
+
+    public static function get(string $key): string
+    {
+        return constant($key);
+    }
 }
